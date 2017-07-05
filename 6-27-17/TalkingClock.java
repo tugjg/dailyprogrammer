259a00e396e5e8ef7ca72ea3d85e4e1bd6493ad7
Jacob <jacobgoldberg22@gmail.com>
2017-07-05 17:08:32 -0400

    file input instead of console input

--- 6-27-17/TalkingClock.java
+++ 6-27-17/TalkingClock.java
@@ -1,17 +1,29 @@
 package TalkingClock;
 import java.util.Scanner;
+import java.io.*;
 public class TalkingClock{
 
     public static final String[] hour = new String[] {"twelve", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven"};
     public static final String[] min = new String[] {"oh ", "twenty ", "thirty ", "forty ", "fifty "};
     public static final String[] min_ext = new String[] {" ", "one ", "two ", "three ", "four ", "five ", "six ", "seven ", "eight ", "nine "};
     public static final String[] min_special = new String[] {"ten ", "eleven ", "twelve ", "thirteen ", "fourteen ", "fifteen ", "sixteen ", "seventeen ", "eighteen ", "nineteen "};
-    public static String[] getUserTime(){
-        Scanner keyboard = new Scanner(System.in);
-        System.out.println("Enter the time in 24 hour format, ex: HR:MN");
-        String userInput = keyboard.nextLine();
-        String[]inputTime = userInput.split(":");
-        return inputTime;
+    public static void getTimes(String inputFile) throws FileNotFoundException{
+        Scanner fileRead = new Scanner(new File(inputFile));
+        int fileLength = 6;
+
+        String[] inputTimeArray = new String[fileLength];
+        for(int i = 0; i<fileLength; i++){
+            inputTimeArray[i]=fileRead.nextLine();
+        }
+
+        for(int i = 0; i<inputTimeArray.length; i++){
+            String[] inputTime = inputTimeArray[i].split(":");
+            inputTimeArray[i] = stringTime(inputTime);            
+        }
+
+        for(int i = 0; i<inputTimeArray.length; i++){
+            System.out.println(inputTimeArray[i]);
+        }
     }
     public static int userInputHr(String[] inputTime){
         int hr = Integer.parseInt(inputTime[0]);
@@ -51,10 +63,10 @@ public class TalkingClock{
                 minuteString+=min[mn_split_int[0]]+min_ext[mn_split_int[1]];
             }
         }else if(mn_split_int[0] == 1){
-             minuteString+=min_special[mn_split_int[1]];
+            minuteString+=min_special[mn_split_int[1]];
         }else{
-             minuteString+=min[mn_split_int[0]-1] +min_ext[mn_split_int[1]];
-            }
+            minuteString+=min[mn_split_int[0]-1] +min_ext[mn_split_int[1]];
+        }
 
         return minuteString;
     }
@@ -72,15 +84,14 @@ public class TalkingClock{
     }
 
 
-    public static String stringTime(){
-        String[] inputTime = getUserTime();
+    public static String stringTime(String[] inputTime){
 
         return("It's "+hourString(inputTime)+ " " +minuteString(inputTime)+amPm(inputTime));
     }
 
 
-    public static void main(String[] args){
-        System.out.println(stringTime());
+    public static void main(String[] args) throws FileNotFoundException{
+        getTimes("input.txt");
     }
 
 }
